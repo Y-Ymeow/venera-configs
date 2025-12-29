e8d996ea2c786bc6d977b0452b0be1cefc00ebc5
Y-Ymeow <ymeow@users.noreply.github.com>
2025-12-29 15:38:41 +0800

    update key

--- godamh.js
+++ godamh.js
@@ -13,7 +13,7 @@ class Goda extends ComicSource {
   minAppVersion = "1.4.0"
 
   // update url
-  url = "https://git.nyne.dev/nyne/venera-configs/raw/branch/main/goda.js"
+  url = "https://git.nyne.dev/nyne/venera-configs/raw/branch/main/godamh.js"
 
   settings = {
     domains: {
